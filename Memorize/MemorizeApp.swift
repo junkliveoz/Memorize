2c40562fd78ca3c5f36ad4aa0a864729dd4532f1
Adam Sayer <asayer82@icloud.com>
2024-08-17 11:22:32 +1000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Adam Sayer on 17/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
