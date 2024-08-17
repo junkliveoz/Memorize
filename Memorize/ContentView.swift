2d65de8c470af87a431f781bd5b28ef86184b683
Adam Sayer <asayer82@icloud.com>
2024-08-17 13:02:42 +1000

    Created the card Layout UI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(1...4, id: \.self) { i in
+                HStack {
+                    ForEach(1...4, id: \.self) { i in
+                    CardView()
+                        .foregroundColor(Color.orange)
+                    }
+                }
+            }
         }
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        if isFaceUp {
+            ZStack {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+        } else {
+            RoundedRectangle(cornerRadius: 12).foregroundColor(.orange)
+        }
     }
 }
 
